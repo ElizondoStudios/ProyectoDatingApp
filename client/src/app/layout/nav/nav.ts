1ddd63cdc20d3531956639295d5624773ddf55c8
Elizondo Studios <elizondostudios@gmail.com>
2025-09-23 07:57:21 -0600

    Integración  de account service con login y logout

--- client/src/app/layout/nav/nav.ts
+++ client/src/app/layout/nav/nav.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,21 @@ import { AccountService } from '../../../core/services/account-service';
   styleUrl: './nav.css'
 })
 export class Nav {
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   protected creds: any = {};
 
   login(): void {
     this.accountService.login(this.creds).subscribe({
-      next: response => console.log(response),
+      next: response => {
+        console.log(response);
+        this.creds = {};
+      },
       error: error => alert(error.message)
     });
   }
+
+  logout(): void {
+    this.accountService.logout();
+  }
 }
+
