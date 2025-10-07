dec3e27b8f314d6d983dc4973d762f5399f297e0
Elizondo Studios <elizondostudios@gmail.com>
2025-10-07 07:26:39 -0600

    Routes view with transitions

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,34 +1,13 @@
-import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { InitService } from '../core/services/init-service';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { Nav } from './layout/nav/nav';
+import { Router, RouterOutlet } from '@angular/router';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient(),
-    provideAppInitializer(async () => {
-      const initService = inject(InitService);
-
-      return new Promise<void>((resolve) => {
-        setTimeout(async () => {
-          try {
-            return lastValueFrom(initService.init())
-          } finally {
-            const splash = document.getElementById("initial-splash");
-
-            if (splash) {
-              splash.remove();
-            }
-
-            resolve();
-          }
-        }, 500);
-      });
-    })
-  ]
-};
+@Component({
+  selector: 'app-root',
+  imports: [Nav, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected router = inject(Router);
+}
