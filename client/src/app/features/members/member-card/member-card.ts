aa3a9c48f3bca865bd0ca70f81e6437fc5273091
Elizondo Studios <elizondostudios@gmail.com>
2025-10-29 07:29:10 -0600

    Estilo a la member Card

--- client/src/app/features/members/member-card/member-card.ts
+++ client/src/app/features/members/member-card/member-card.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Member } from '../../../../types/member';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-member-card',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './member-card.html',
   styleUrl: './member-card.css'
 })
